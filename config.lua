3f176a4448b1514198a4a915c1ba14497a96d9b3
AboMalak <malekalsobhi38@gmail.com>
2025-10-06 18:43:25 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,6 @@
 Config = {}
 
-Config.Debug = true -- set to false to disable debug prints
+Config.Debug = false -- set to false to disable debug prints
+
 
 Config.Enabled = true -- set to false to disable the script
